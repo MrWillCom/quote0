f50dae880373bfcaa1ec6652ed0d068f9c61596c
Mr. Will <mr.will.com@outlook.com>
2026-02-21 18:10:58 +0800

    Alr, remove Effect

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import { Effect, Console } from 'effect'
+import 'dotenv/config'
 
-const program = Console.log('Hello, World!')
+async function main() {
+  const url =
+    'https://dot.mindreset.tech/api/authV2/open/device/' +
+    process.env.QUOTE0_DEVICEID +
+    '/status'
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + process.env.QUOTE0_API_KEY,
+    },
+  }
 
-Effect.runSync(program)
+  try {
+    const response = await fetch(url, options)
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+await main()
