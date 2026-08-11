a559bb0a9ae7ea5e09da441f69703c1f3c1848a3
Mr. Will <mr.will.com@outlook.com>
2026-08-11 15:02:08 +0800

    Migrate to Hey API

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: ['src/index.tsx', 'src/api/index.ts'],
+  entry: [
+    // temporarily stop building src/index
+    // 'src/index.tsx',
+    'src/client/index.ts',
+  ],
   format: ['esm'],
   clean: true,
   minify: true,
   dts: true,
   target: false,
   unbundle: true,
+  fixedExtension: false,
 })
