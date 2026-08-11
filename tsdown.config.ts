abfe23ab5725114a1aa4d52e0b2ae93b0b17dee3
Mr. Will <mr.will.com@outlook.com>
2026-08-11 15:35:10 +0800

    Set up Turborepo

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
+  root: 'src',
   entry: [
     // temporarily stop building src/index
     // 'src/index.tsx',
     'src/client/index.ts',
+    'src/client/client.gen.ts',
   ],
-  format: ['esm'],
-  clean: true,
-  minify: true,
-  dts: true,
-  target: false,
   unbundle: true,
   fixedExtension: false,
+  sourcemap: true,
+  target: false,
+  publint: true,
+  attw: {
+    profile: 'esm-only',
+  },
 })
